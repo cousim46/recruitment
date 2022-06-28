e0f4f8b8fbd162ed6e04db64f52bca1f035afe3a
cousim46 <wjdghldns24@naver.com>
2022-06-28 22:53:05 +0900

    review entity

---
+++ src/main/java/com/example/triple/entity/review/Review.java
@@ -0,0 +1,29 @@
+package com.example.triple.entity.review;
+
+import com.example.triple.entity.place.Place;
+import com.example.triple.entity.user.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Review {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)",name = "reviewId")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "placeId")
+    private Place place;
+}
