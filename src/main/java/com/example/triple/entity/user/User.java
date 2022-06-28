8e51f8a622bd95b9cad0202e71071b4faa01a22c
cousim46 <wjdghldns24@naver.com>
2022-06-28 22:53:14 +0900

    user entity

---
+++ src/main/java/com/example/triple/entity/user/User.java
@@ -0,0 +1,28 @@
+package com.example.triple.entity.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "user_entity")
+public class User {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)",name = "userId")
+    private UUID id;
+    @Column(columnDefinition = "VARCHAR(165)")
+    private String username;
+    private int point;
+
+    public User(String username) {
+        this.username = username;
+    }
+}
