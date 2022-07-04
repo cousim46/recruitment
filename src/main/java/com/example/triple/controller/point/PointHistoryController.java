9b25aa427b318b55b5bc7b07272ab58e1d01705d
cousim46 <wjdghldns24@naver.com>
2022-07-04 22:02:24 +0900

    포인트에 관한 Controller

---
+++ src/main/java/com/example/triple/controller/point/PointHistoryController.java
@@ -0,0 +1,31 @@
+package com.example.triple.controller.point;
+
+import com.example.triple.dto.events.EventsResponse;
+import com.example.triple.dto.point.PointHistoryResponse;
+import com.example.triple.service.point.PointHistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class PointHistoryController {
+    private final PointHistoryService pointHistoryService;
+
+
+    @PostMapping("/events")
+    public void events(@RequestBody EventsResponse eventsResponse) {
+
+        pointHistoryService.events(eventsResponse);
+    }
+
+
+    @GetMapping("/point-history")
+    public ResponseEntity<Slice<PointHistoryResponse>> userPointHistory(@RequestParam(name = "userid") UUID userId) {
+        return new ResponseEntity<>(pointHistoryService.userPointHistory(userId), HttpStatus.OK);
+    }
+}
