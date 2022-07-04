cb943187f4b0e1cfb33672d6512d9e22c81c2d8c
cousim46 <wjdghldns24@naver.com>
2022-07-04 22:03:25 +0900

    리뷰에 관한 Repository

--- src/main/java/com/example/triple/repository/review/ReviewRepository.java
+++ src/main/java/com/example/triple/repository/review/ReviewRepository.java
@@ -1,9 +1,19 @@
 package com.example.triple.repository.review;
 
 import com.example.triple.entity.review.Review;
+import com.example.triple.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
+
+    @Query("select r from  Review r where r.id=:reviewId and r.user = :user")
+    Review findReview(UUID reviewId, User user);
+
+    void deleteByIdAndUser(UUID reviewId, User user);
+
+    @Query("select count(r) from Review r where r.placeId = :placeId")
+    Long countPlace(UUID placeId);
 }
