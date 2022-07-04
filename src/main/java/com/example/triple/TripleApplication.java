32b6e779bef1ac485d3a423a6d6a5fc35a05cde8
cousim46 <wjdghldns24@naver.com>
2022-07-04 22:04:08 +0900

    @EnableJpaAuditing 추가

--- src/main/java/com/example/triple/TripleApplication.java
+++ src/main/java/com/example/triple/TripleApplication.java
@@ -2,12 +2,14 @@ package com.example.triple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TripleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TripleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TripleApplication.class, args);
+    }
 
 }
