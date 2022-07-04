7e609e92a4ece49429b5e0825683bb1d4abf21ba
cousim46 <wjdghldns24@naver.com>
2022-07-04 18:13:09 +0900

    user에 포인트 부여

--- src/main/java/com/example/triple/entity/user/User.java
+++ src/main/java/com/example/triple/entity/user/User.java
@@ -1,5 +1,6 @@
 package com.example.triple.entity.user;
 
+import com.example.triple.entity.review.Review;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
@@ -11,7 +12,7 @@ import java.util.UUID;
 @Entity
 @NoArgsConstructor
 @Getter
-@Table(name = "user_entity")
+@Table(name = "user_table")
 public class User {
     @Id
     @GeneratedValue(generator = "uuid2")
@@ -21,9 +22,37 @@ public class User {
     private UUID id;
     @Column(columnDefinition = "VARCHAR(165)")
     private String username;
-    private int point;
+    private int currentPoint;
 
     public User(String username) {
         this.username = username;
+    }
+
+    public void updateCurrentPoint(int currentPoint) {
+        this.currentPoint = currentPoint;
+    }
+
+    public void givePoint(Review review, int imageCount, Long placeCount) {
+        int contentPoint = 0;
+        int imagePoint = 0;
+        int specialPlacePoint = 0;
+        int totalPoint = 0;
+        if (review.getContent().length() >= 1) {
+            contentPoint = 1;
+        }
+        if (imageCount >= 1) {
+            imagePoint = 1;
+        }
+        if (placeCount == 0) {
+            specialPlacePoint = 1;
+        }
+        totalPoint = contentPoint + imagePoint + specialPlacePoint;
+        review.increaseOrDecreasePoint(contentPoint, imagePoint, specialPlacePoint, totalPoint);
+
+        this.currentPoint = this.currentPoint + totalPoint;
+    }
+
+    public void decreasePoint(int point) {
+        this.currentPoint -= point;
     }
 }
