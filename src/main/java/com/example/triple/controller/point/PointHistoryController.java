93978688cd491906d0d7627d4015dc3796edb2e4
cousim46 <wjdghldns24@naver.com>
2022-07-05 14:03:52 +0900

    events API 반환값 변경

--- src/main/java/com/example/triple/controller/point/PointHistoryController.java
+++ src/main/java/com/example/triple/controller/point/PointHistoryController.java
@@ -2,6 +2,7 @@ package com.example.triple.controller.point;
 
 import com.example.triple.dto.events.EventsResponse;
 import com.example.triple.dto.point.PointHistoryResponse;
+import com.example.triple.dto.review.ReviewResponse;
 import com.example.triple.service.point.PointHistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
@@ -19,9 +20,9 @@ public class PointHistoryController {
 
 
     @PostMapping("/events")
-    public void events(@Validated @RequestBody EventsResponse eventsResponse) {
+    public ResponseEntity<ReviewResponse> events(@Validated @RequestBody EventsResponse eventsResponse) {
 
-        pointHistoryService.events(eventsResponse);
+        return new ResponseEntity<>(pointHistoryService.events(eventsResponse), HttpStatus.OK);
     }
 
 
